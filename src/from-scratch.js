06e71021555623241554dbdd2f472ed7dcc62a3f
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-04-09 15:02:01 -0400

    Assignmemt completed

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,21 +1,54 @@
 const loop0UpTo10 = () => {
+  for (let i = 0; i < 10; i++) {
+    console.log(i)
+  }
 };
+loop0UpTo10()
 
 const loop5to10 = () => {
+  for (let i = 5; i <= 10; i++)
+    console.log(i)
 };
+console.log("-----")
+loop5to10()
 
 // you do NOT need an if check
 const loopEvenNumbersUpTo10 = () => {
+  for (let i = 0; i < 10; i += 2) {
+    console.log(i)
+  }
 };
+console.log("-----")
+loopEvenNumbersUpTo10()
 
 const countdown5to0 = () => {
+  for (let i = 5; i >= 0; i--) {
+    console.log(i)
+  }
 };
+console.log("-----")
+countdown5to0()
 
-const loopUpToNum = () => {
+const loopUpToNum = (num) => {
+  for (i = 0; i < num; i++) {
+    console.log(i)
+  }
 };
+loopUpToNum(9)
+
 
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++)
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else if (i % 3 === 0) {
+      console.log("fizz")
+    } else console.log(i)
 };
+fizzbuzz(15)
+
 
 module.exports = {
   loop0UpTo10,
